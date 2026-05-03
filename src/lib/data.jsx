dff47a6285528e45baa1e0417567fd3edf05f194
ashik081 <freelancerashik81@gmail.com>
2026-05-03 11:05:34 +0600

    fixed some bugs for deployment issuse

--- src/lib/data.jsx
+++ src/lib/data.jsx
@@ -1,21 +1,29 @@
 export const booksDataFetch = async () => {
-  const res = await fetch("http://localhost:3000/books.json");
+  const res = await fetch("http://localhost:3000/books.json", {
+    cache: "no-store",
+  });
   const data = await res.json();
   return data.books;
 };
 export const testimonialsDataFetch = async () => {
-  const res = await fetch("http://localhost:3000/testimonials.json");
+  const res = await fetch("http://localhost:3000/testimonials.json", {
+    cache: "no-store",
+  });
   const data = await res.json();
   return data;
 };
 export const statsDataFetch = async () => {
-  const res = await fetch("http://localhost:3000/stats.json");
+  const res = await fetch("http://localhost:3000/stats.json", {
+    cache: "no-store",
+  });
   const data = await res.json();
   return data;
 };
 
 export const categoriesDataFetch = async () => {
-  const res = await fetch("http://localhost:3000/category.json");
+  const res = await fetch("http://localhost:3000/category.json", {
+    cache: "no-store",
+  });
   const data = await res.json();
   return data.categories;
 };
